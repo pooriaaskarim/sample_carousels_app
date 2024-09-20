6a57a2205d174f2ae75226736fb35ce41ed20760
pooriaaskarim <pooriaaskarim@gmail.com>
2024-09-21 03:27:27 +0330

    + Configured App

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,43 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget{
+import 'presentation/common/theme/app.theme.dart';
+import 'presentation/screens/products/screen.products.dart';
+
+class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Sample Carousels App',
-    );
-  }}
+  Widget build(final BuildContext context) => SafeArea(
+        maintainBottomViewPadding: true,
+        child: MaterialApp(
+          title: 'Sample Carousels App',
+          home: const ProductsScreen(),
+          theme: AppThemes.lightTheme,
+          darkTheme: AppThemes.darkTheme,
+          scrollBehavior: const MaterialScrollBehavior().copyWith(
+            scrollbars: false,
+            dragDevices: {
+              PointerDeviceKind.mouse,
+              PointerDeviceKind.touch,
+            },
+          ),
+          debugShowCheckedModeBanner: false,
+        ),
+        // child: MaterialApp.router(
+        //   routerConfig: AppRouter.router,
+        //   title: 'Sample Carousels App',
+        //   theme: AppThemes.lightTheme,
+        //   darkTheme: AppThemes.darkTheme,
+        //   scrollBehavior: const MaterialScrollBehavior().copyWith(
+        //     scrollbars: false,
+        //     dragDevices: {
+        //       PointerDeviceKind.mouse,
+        //       PointerDeviceKind.touch,
+        //     },
+        //   ),
+        //   debugShowCheckedModeBanner: false,
+        // ),
+      );
+}
